e67e49534e94e5d93084368650e83ec2beb01024
Antonio-dp <antonio.delpardo229161@potros.itson.edu.mx>
2023-05-19 00:48:41 -0700

    Update proyectoFinal.ino

--- proyectoFinal/proyectoFinal.ino
+++ proyectoFinal/proyectoFinal.ino
@@ -3,7 +3,12 @@
 
    https://wokwi.com/arduino/projects/322410731508073042
 */
-
+/** Estas son las librerías que se están incluyendo en el código. 
+    Las librerías son conjuntos de funciones predefinidas que facilitan la 
+    programación al proporcionar implementaciones de funciones comunes y útiles. 
+    En este caso, estas librerías son necesarias para diversas funcionalidades del programa, 
+    como la comunicación en red, control de periféricos y manejo de tiempo.
+*/
 #include <NoDelay.h>
 #include <string.h>
 #include <NewPing.h>
@@ -16,60 +21,79 @@
 #include <NTPClient.h>
 #include "time.h"
 
-//Constantes de red
+/* Aquí se definen algunas constantes relacionadas con la configuración de la red Wi-Fi. 
+  ssid y password representan el nombre y la contraseña de la red Wi-Fi a la que se desea conectar el dispositivo. 
+  contrasena es una variable de tipo String que almacena la contraseña. 
+  AsyncWebServer es un objeto que se utiliza para configurar y manejar un servidor web asíncrono en el puerto 80.
+*/
 const char* ssid = "A75B0C";
 const char* password = "K2T76D2C03543";
+//Contraseña de acceso por defecto
 String contrasena = "ea";
+//Inicia el servidor web en el puerto 80
 AsyncWebServer server(80);
 
-//Constantes del sensor ultrasonico
+/*
+  Aquí se definen constantes relacionadas con un sensor ultrasónico. 
+  PIN_TRIGGER y PIN_ECHO representan los pines utilizados para conectar el sensor ultrasónico. 
+  BAUD_RATE indica la velocidad de comunicación en baudios. DISTANCIA_MAX y DISTANCIA_ALERTA 
+  son valores límite para la distancia medida por el sensor ultrasónico. 
+  limiteInferior y limiteSuperior son límites utilizados para ciertas operaciones en el código.
+*/
 const unsigned int PIN_TRIGGER = 5;
 const unsigned int PIN_ECHO = 18;
+//BAUD_RATE indica la velocidad de comunicación en baudios
 const unsigned int BAUD_RATE = 115200;
-const int DISTANCIA_MAX = 100;
-const int DISTANCIA_ALERTA = 10;
+//Limite inferior en cm de deteccion del sensor ultrasonico
 long limiteInferior = 10;
+//Limite superior en cm de deteccion del sensor ultrasonico
 long limiteSuperior = 11;
 
-//Constantes del servomotor
+//Pin de informacion del servo
 const unsigned int PIN_SERVO = 13;
-const long PAUSA_SERVO = 250;
 
 //Constante de potenciometro
 const unsigned int PIN_POT = 34;
 
-//Constantes de los leds
+//Pin led rojo
 const int PIN_LED_ROJO = 2;
+//Pin led verde
 const int PIN_LED_VERDE = 4;
+//Pin led blanco
 const int PIN_LED_BLANCO = 17;
+
+//Variable que permite regular la intensidad del brillo de el led blanco
 int brillo;
+//Variable que guarda el valor de resistencia del potenciometro
 int pot = 0;
 
+//Permite guardar el tiempo actual en ms
 unsigned long tiempoActual = 0;
+//Permite guardar el tiempo anterior en ms
 unsigned long tiempoAnterior = 0;
+//Permite guardar el intervalo en ms
 unsigned long intervalo = 10;
+//Pausa en ms
 const long PAUSA = 500;
-//python
+//Permite guardar el objeto json a enviar en el POST para python
 char salidaJson[128];
 
+//Enum de los estados del sistema
 typedef enum {
   DESOCUPADO,
   OCUPADO,
   OCUPADOYESPERANDO
 } estadoSala;
-
+//Variable de estado del sistema
 estadoSala edo;
-
+//Inicializado de la variable de pausa
 noDelay pausa(PAUSA);
 
-//Variables del sensor ultrasonico
+//Inicializado de la variable que permite el funcionamiento del sensor ultrasonico
 NewPing sonar(PIN_TRIGGER, PIN_ECHO, DISTANCIA_MAX);
 
 //Variables del servomotor
 Servo servo;
-
-//Variables del zumbador
-int frecuencia;
 
 //variables de tiempo
 // URL de un servidor NTP (Network Time Protocol)
@@ -81,31 +105,54 @@ const int daylightOffset_sec = 0;
 // Estructura con la informacion de la fecha/hora actual
 struct tm timeinfo;
 
-void apagarAlarma();
+
+//Permite regular la intensidad de la luz del led blanco
 void controlarLuzLedBlanco();
+
+//Enciende el led rojo y apaga el LED Verde
 void encenderOcupado();
+
+//Enciende el led verde y apaga el LED Rojo
 void encenderDesocupado();
+
+//Gira el servomotor 60 grados para bloquear la puerta
 void servoBloqueado();
+
+//Gira el servomotor 180 grados para desbloquear la puerta
 void servoDesbloqueado();
+
+//Permite obtener la distancia del objeto con el sensor ultrasonico
 long distanciaUnPing();
+
+//Permite insertar el estado que se utilizara en la pagina web
 String processor(const String& var);
+//Devuelve el estado actual en string
 String sacarEstado();
 
-//python
+//Permite enviar el estado atual y la fecha con un método POST
 void peticionesPython();
 
+//Permite desbloquear el servomotor despues de 3 segundos
 noDelay pausaAbierto(3000, servoBloqueado, false);
-//Servidor ESP32
+
+//Permite conectarse a la red wifi
 void conectaRedWifi(const char* ssid, const char* password);
+
+//Servidor web del ESP32
 void configuraServidor();
+
+//Envia una respuesta si ocurre un fallo en la petición
 void noHallada(AsyncWebServerRequest* request);
 
 void setup() {
+  //Inicializa el servomotor
   servo.attach(PIN_SERVO);
+  //Inicia la velocidad de comunicación en baudios
   Serial.begin(BAUD_RATE);
 
-  //wifi
+  //Se conecta a la red
   conectaRedWifi(ssid, password);
+  //Inicializa el servidor y habilita los endpoints
   configuraServidor();
   server.begin();
   Serial.println("Servidor web iniciado");
@@ -113,22 +160,25 @@ void setup() {
   //Hora y fecha
   configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
 
-  //leds
+  //Configura el PIN y los LEDS
   pinMode(PIN_LED_ROJO, OUTPUT);
   pinMode(PIN_LED_VERDE, OUTPUT);
   pinMode(PIN_LED_BLANCO, OUTPUT);
 
+  //Inicia sin seguro la puerta y con el LED Verde encendida
   encenderDesocupado();
   servoDesbloqueado();
   edo = DESOCUPADO;
 }
 
 void loop() {
+  //Permite controlar la luz
   tiempoActual = millis();
   if (tiempoActual - tiempoAnterior >= intervalo) {
     tiempoAnterior = tiempoActual;
     controlarLuzLedBlanco();
   }
+  //Permite saber si hay una persona cerca mientras el estado sea OCUPADO
   if ((intptr_t)distanciaUnPing() < limiteInferior && (intptr_t)distanciaUnPing() != 0) {
     if (edo == OCUPADO) {
       edo = OCUPADOYESPERANDO;
@@ -138,41 +188,42 @@ void loop() {
       edo = OCUPADO;
     }
   }
+  //Permite cerrar la puerta despues de abrirla con contraseña
   if (pausaAbierto.update()) {
     pausaAbierto.stop();
   }
 }
 
-
+//Gira el servomotor 60 grados para bloquear la puerta
 void servoBloqueado() {
   servo.write(60);
 }
-
+//Gira el servomotor 180 grados para desbloquear la puerta
 void servoDesbloqueado() {
   servo.write(180);
 }
-
+//Enciende el led rojo y apaga el LED Verde
 void encenderOcupado() {
   digitalWrite(PIN_LED_ROJO, HIGH);
   digitalWrite(PIN_LED_VERDE, LOW);
 }
-
+//Enciende el led verde y apaga el LED Rojo
 void encenderDesocupado() {
   digitalWrite(PIN_LED_ROJO, LOW);
   digitalWrite(PIN_LED_VERDE, HIGH);
 }
-
+//Permite regular la intensidad de la luz del led blanco
 void controlarLuzLedBlanco() {
   pot = analogRead(PIN_POT);
   brillo = map(pot, 0, 4095, 0, 255);
   analogWrite(PIN_LED_BLANCO, brillo);
 }
-
+//Permite obtener la distancia del objeto con el sensor ultrasonico
 long distanciaUnPing() {
   int distancia = sonar.ping_cm();
   return distancia;
 }
-
+//Permite conectarse a la red wifi
 void conectaRedWifi(const char* ssid, const char* password) {
   Serial.print("Conectando..");
   WiFi.begin(ssid, password);
@@ -183,16 +234,21 @@ void conectaRedWifi(const char* ssid, const char* password) {
   Serial.println(WiFi.localIP());
 }
 
+//Servidor web del ESP32
 void configuraServidor() {
+  //EndPoint get que lanza la pagina de inicio en donde puedes introducir la contraseña para desbloquear la puerta
   server.on("/", HTTP_GET, [](AsyncWebServerRequest* request) {
     request->send_P(200, "text/html", inicio, processor);
   });
+  //EndPoint get que lanza la pagina de inicio en donde puedes cambiar entre el estado de OCUPADO y DESOCUPADO
   server.on("/bloqueo", HTTP_GET, [](AsyncWebServerRequest* request) {
     request->send_P(200, "text/html", bloqueo, processor);
   });
+  //EndPoint get que lanza la pagina de inicio en donde puedes cambiar la contraseña con la que desbloqueas la puerta
   server.on("/contra", HTTP_GET, [](AsyncWebServerRequest* request) {
     request->send_P(200, "text/html", contra, processor);
   });
+  //Permite activar las funciones para bloquear la puerta y encender el led rojo
   server.on("/bloquear", HTTP_GET, [](AsyncWebServerRequest* request) {
     encenderOcupado();
     servoBloqueado();
@@ -201,6 +257,7 @@ void configuraServidor() {
     const char content[] PROGMEM = "todo fine";
     request->send_P(200, "text/plain", content);
   });
+  //Permite activar las funciones para desbloquear la puerta y encender el led verde
   server.on("/desbloquear", HTTP_GET, [](AsyncWebServerRequest* request) {
     encenderDesocupado();
     servoDesbloqueado();
@@ -209,6 +266,7 @@ void configuraServidor() {
     const char content[] PROGMEM = "todo fine";
     request->send_P(200, "text/plain", content);
   });
+  //Permite abrir la puerta con el codigo dado en la pagina
   server.on("/code", HTTP_GET, [](AsyncWebServerRequest* request) {
     String codigo = request->getParam("codigo")->value();
     if (codigo == contrasena) {
@@ -224,6 +282,7 @@ void configuraServidor() {
     const char content[] PROGMEM = "todo fine";
     request->send_P(200, "text/plain", content);
   });
+  //Permite cambiar la contraseña para desbloquear la puerta por la dada en la petición
   server.on("/password", HTTP_GET, [](AsyncWebServerRequest* request) {
     String passw = request->getParam("password")->value();
     contrasena = passw;
@@ -232,7 +291,7 @@ void configuraServidor() {
   });
   server.onNotFound(noHallada);
 }
-
+//Devuelva una respuesta en caso de algun error en la petición
 void noHallada(AsyncWebServerRequest* request) {
   // Le envia al cliente el mensaje de respuesta. Recibe como
   // argumentos el codigo de estado HTTP: 200 (indica exito al
@@ -241,17 +300,25 @@ void noHallada(AsyncWebServerRequest* request) {
   // una cadena con la pagina con el mensaje "URL no encontrada".
   request->send(404, "text/plain", "URL no encontrada");
 }
-
+//Cliente que permite hacer POST desde el ESP32 a una api hosteada.
 void peticionesPython() {
+  //Variable de fecha
   time_t now;
+  //Cliente http
   HTTPClient http;
+  //URL de la api hosteada
   http.begin("https://lock-system-production.up.railway.app/datos");
+  //header
   http.addHeader("Content-Type", "application/json");
+  //Permite obtener la fecha y hora actual
   if (!getLocalTime(&timeinfo)) {
     Serial.println("No se pudo obtener la fecha/hora");
   }
+  //Setea la fecha y hora actual en a variable now
   time(&now);
+  //Establece el tamaño de objetos en el json document
   const size_t capacity = JSON_OBJECT_SIZE(2);
+  //Json document
   StaticJsonDocument<capacity> doc;
   switch (edo) {
     case OCUPADO:
@@ -265,7 +332,9 @@ void peticionesPython() {
       break;
   }
   doc["fecha"] = now;
+  //Serializacion de los datos a enviar
   serializeJson(doc, salidaJson);
+  //Peticion POST
   int httpCode = http.POST(String(salidaJson));
   if (httpCode > 0) {
     Serial.println("Codigo HTTP" + String(httpCode));
@@ -276,13 +345,13 @@ void peticionesPython() {
     }
   }
 }
-
+//Permite insertar el estado que se utilizara en la pagina web
 String processor(const String& var) {
   if (var == "ESTADO")
     return sacarEstado();
   return String();
 }
-
+//Devuelve el estado actual en string
 String sacarEstado() {
   switch (edo) {
     case OCUPADO:
